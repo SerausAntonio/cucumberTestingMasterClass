67e8ac8f2d51e3397be3aa98b745c23fb6636e2b
Antonio Seraus <aseraus@hotmail.com>
2022-04-24 21:15:39 +0200

    first test added

---
+++ src/main/java/Application.java
@@ -0,0 +1,32 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Application {
+    static  WebDriver driver;
+    public static void main(String[] args) {
+        System.out.println("Hello i am here");
+        System.out.println(System.getProperty("user.dir"));
+
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("http://www.wikipedia.org");
+
+       System.out.println(driver.getTitle());
+
+        System.out.println(driver.findElement(By.xpath("//*[@class='central-textlogo-wrapper']")).getText());
+        driver.findElement(By.id("js-link-box-en")).click();
+        System.out.println(driver.findElement(By.xpath("//*[@class='mw-headline']")).getText());
+
+        driver.quit();
+
+
+    }
+}
+
+
